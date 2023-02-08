9ef4c2a804459901e40490001a88d400cbbcbe78
Sadegh Ghasemi <82932863+Sadegh-AT@users.noreply.github.com>
2023-02-08 15:54:47 +0330

    add some comment to Script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Slider Constractor
 var swiper = new Swiper(".mySlider", {
   loop: true,
   slidesPerView: 1,
@@ -7,17 +8,17 @@ var swiper = new Swiper(".mySlider", {
     clickable: true,
   },
   breakpoints: {
-    // when window width is >= 320px
+    // when window width is >= 576px
     576: {
       slidesPerView: 1,
       spaceBetween: 20,
     },
-    // when window width is >= 480px
+    // when window width is >= 768px
     768: {
       slidesPerView: 2,
       spaceBetween: 30,
     },
-    // when window width is >= 640px
+    // when window width is >= 992px
     992: {
       slidesPerView: 3,
       spaceBetween: 40,
